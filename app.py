4ebb2e9737f44ae0486c2f7c530389181e9e6445
Dhruba Basumatary <dhru@duck.com>
2023-04-16 20:43:07 +0530

    fixed upload issues & added app.py for streamlit host

--- app.py
+++ app.py
@@ -1,61 +1,24 @@
-# Importing necessary modules
-from huggingface_hub import notebook_login
-from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
-import torch
-from PIL import Image
-import matplotlib.pyplot as plt
-import requests
-import openai
+import streamlit as st
+from main import generate_caption
+
+st.title('GPT Caption : Assignment Demo')
+st.header('Automatically generate captions for your images!')
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Display the image
+    st.image(uploaded_file, use_column_width=True)
+
+    # Generate caption
+    #caption = generate_caption(uploaded_file)
+    caption, text_snippet = generate_caption(uploaded_file)
 
 
-# Loading pre-trained model and tokenizer
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    # Display caption
+    st.subheader('Caption:')
+    st.write(caption)
 
-# Print types of model, feature_extractor, and tokenizer
-print(type(model))
-print(type(feature_extractor))
-print(type(tokenizer))
-
-
-# Load ViTImageProcessor to convert image to PyTorch tensor
-feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-#print(feature_extractor)
-
-
-# Setting the device to use cuda if it is available, otherwise use cpu
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Moving the model to the device
-model.to(device)
-
-
-def generate_caption(image):
-    # Converting the image to PyTorch tensor and moving it to the device
-    pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
-    pixel_values = pixel_values.to(device)
-
-    # Generating a caption for the image using the pre-trained model
-    generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-    generated_caption = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-    # Returning the generated caption
-    return generated_caption
-
-
-# Open the image file and convert it to an RGB image
-image = Image.open('/home/xai/Lab/XD/transformers/Image3-min.png')
-image = image.convert('RGB')
-
-# Create a PyTorch tensor from the image data and reshape it to match the size of the input image
-tensor = torch.Tensor(list(image.getdata())).view(image.size[1], image.size[0], 3)
-
-# Unsqueeze the tensor along the 0th dimension to create a batch of size 1
-tensor = tensor.unsqueeze(0)
-
-# Call the `generate_caption()` function with the `image` tensor as input and assign the generated caption to the variable `caption`
-caption = generate_caption(image)
-
-# Print the generated caption to the console
-print("CAPTION 👉", caption)
+    # Display text snippet
+    st.subheader('Text snippet for social media:')
+    st.write(text_snippet)
--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoToken
 from PIL import Image
 from tkinter import filedialog, Tk
 
+#load openai api key
+openai.api_key = config.api_key
+
+
 
 # Loading pre-trained model and tokenizer
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
@@ -25,16 +29,38 @@ else:
 # Moving the model to the device
 model.to(device)
 
-def generate_caption(image):
+def generate_caption(image_file):
     try:
+        # Open the image file and convert it to an RGB image
+        image = Image.open(image_file).convert('RGB')
         # Converting the image to PyTorch tensor and moving it to the device
         pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values.to(device)
         # Generating a caption for the image using the pre-trained model
         generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
         generated_caption = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        return generated_caption
+        #return generated_caption
+    #except Exception as e:
+        #raise Exception("An error occurred while generating the caption:", e)
+
+    # Generate text snippet using OpenAI API
+        prompt = f"As an expert social media influencer, write three captions for this image on Instagram that will engage your audience:\n{generated_caption}\nInclude relevant hashtags and tag any relevant individuals or brands to increase engagement."
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=100,
+            n=4,
+            stop=None,
+            temperature=0,
+        )
+        # Extract generated text snippet from the OpenAI response
+        generated_text = response.choices[0].text.strip()
+        # Wrap the generated caption into multiple lines
+        wrapped_text = textwrap.fill(generated_text, width=70)
+        # Return the generated caption and text snippet
+        return generated_caption, wrapped_text
     except Exception as e:
         raise Exception("An error occurred while generating the caption:", e)
+
 
 def open_image():
     # Create a tkinter root window (required for file dialog)
@@ -62,41 +88,31 @@ def main():
             caption = generate_caption(image)
             print("Image file selected:", file_path)
             print("CAPTION 👉", caption)
+            
+            # Generate text snippet using OpenAI API
+            prompt = f"As an expert social media influencer, write three caption for this image on Instagram that will engage your audience:\n{caption}\nInclude relevant hashtags and tag any relevant individuals or brands to increase engagement."
+
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=100,
+                n=4,
+                stop=None,
+                temperature=0,
+            )
+
+            # Extract generated text snippet from the OpenAI response
+            generated_text = response.choices[0].text.strip()
+
+            # Wrap the generated caption into multiple lines
+            wrapped_text = textwrap.fill(generated_text, width=70)
+
+            # Print the wrapped text
+            print("\nGenerated caption:\n")
+            print(wrapped_text)
+            
         except Exception as e:
             print(e)
 
 if __name__ == "__main__":
-    main()
-
-openai.api_key = config.api_key
-# Generate caption for the image
-
-image = Image.open(file_path).convert('RGB')
-
-
-caption = generate_caption(image)#(url)
-
-
-# Generate text snippet using OpenAI API
-prompt = f"As an expert social media influencer, write three caption for this image on Instagram that will engage your audience:\n{caption}\nInclude relevant hashtags and tag any relevant individuals or brands to increase engagement."
-
-response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=prompt,
-    max_tokens=100,
-    n=4,
-    stop=None,
-    temperature=0,
-)
-
-# Extract generated text snippet from the OpenAI response
-generated_text = response.choices[0].text.strip()
-
-# Wrap the generated caption into multiple lines
-wrapped_text = textwrap.fill(generated_text, width=70)
-
-# Print the generated text
-#print("Generated caption:", generated_text)
-# Print the wrapped text
-print("Generated caption:\n")
-print(wrapped_text)
+ main()
